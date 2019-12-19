21630242bbdf45f432eb44aa05a2996f267a9d97
Jonas Wessman Nielsen <5615825@RTS-365.dk>
2019-12-19 14:29:46 +0100

    nodejs fetch done

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 let myFetch = require("./umd");
 
-myFetch.get();
+
+
+myFetch.init({
+    address: "https://reqres.in/api/",
+    key: "1234"
+});
+
+myFetch.get("1").then(result => console.log(result));
--- umd.js
+++ umd.js
@@ -1,15 +1,15 @@
-(function(root, factory) {
+(function (root, factory) {
 	// AMD
 	if (typeof define === "function" && define.amd) {
 		define(["jquery"], factory);
-	// CommonJS
+		// CommonJS
 	} else if (typeof exports === "object") {
 		module.exports = factory(require("jquery"));
-	// Browser context
+		// Browser context
 	} else {
 		root.myFetch = factory(root.jquery);
 	}
-}(this, function($) {
+})(this, function ($) {
 	// vores egen del af modulet
 	function init(options) {
 		this.APIAddress = options.address;
@@ -17,93 +17,126 @@
 		return this;
 	}
 
+	// get
 	async function get(resource) {
 		try {
 			if (typeof fetch === "function") {
 				let response = await fetch(this.APIAddress + resource, {
 					headers: {
-						"Authorization": this.APIKey
+						Authorization: this.APIKey
 					}
 				});
-	
+
 				return await response.json();
-			} else {
+			} else if (typeof XMLHttpRequest === "function") {
 				let xhttp = new XMLHttpRequest();
 				xhttp.open("GET", this.APIAddress + resource, true);
 				xhttp.send();
-				return await new Promise(function(resolve, reject) {
-					xhttp.onreadystatechange = function() {
+				return await new Promise(function (resolve, reject) {
+					xhttp.onreadystatechange = function () {
 						if (this.readyState == 4 && this.status == 200) {
 							resolve(JSON.parse(xhttp.responseText));
 						}
 					};
 				});
+			} else {
+				const fetch = require("node-fetch");
 
+				let response = await fetch(this.APIAddress + resource, {
+					headers: {
+						Authorization: this.APIKey
+					}
+				});
+
+				return response.json();
 			}
-
 		} catch (error) {
 			throw error;
 		}
 	}
 
+	// create
 	async function post(resource, data) {
 		try {
 			if (typeof fetch === "function") {
 				let response = await fetch(this.APIAddress + resource, {
 					headers: {
-						"Authorization": this.APIKey,
+						Authorization: this.APIKey,
 						"Content-Type": "application/json"
 					},
 					method: "POST",
 					body: JSON.stringify(data)
 				});
-	
+
 				return await response.json();
-			} else {
+			} else if (typeof XMLHttpRequest === "function") {
 				let xhttp = new XMLHttpRequest();
 				xhttp.open("POST", this.APIAddress + resource, true);
 				xhttp.setRequestHeader("Content-Type", "application/json");
 				xhttp.setRequestHeader("Authorization", this.APIKey);
 				xhttp.send(JSON.stringify(data));
-				return await new Promise(function(resolve, reject) {
-					xhttp.onreadystatechange = function() {
+				return await new Promise(function (resolve, reject) {
+					xhttp.onreadystatechange = function () {
 						if (this.readyState == 4 && this.status == 201) {
 							resolve(JSON.parse(xhttp.responseText));
 						}
 					};
 				});
+			} else {
+				const fetch = require("node-fetch");
+
+				let response = await fetch(this.APIAddress + resource, {
+					headers: {
+						Authorization: this.APIKey,
+						"Content-Type": "application/json"
+					},
+					method: "POST",
+					body: JSON.stringify(data)
+				});
+
+				return response.json();
 			}
 		} catch (error) {
 			throw error;
 		}
 	}
 
+	// delete
 	async function del(resource) {
 		try {
 			if (typeof fetch === "function") {
 				let response = await fetch(this.APIAddress + resource, {
 					headers: {
-						"Authorization": this.APIKey
+						Authorization: this.APIKey
 					},
 					method: "DELETE"
 				});
-	
-				return await new Promise(function(resolve, reject) {
+
+				return await new Promise(function (resolve, reject) {
 					resolve(response.status);
 				});
-
-			} else {
+			} else if (XMLHttpRequest === "function") {
 				let xhttp = new XMLHttpRequest();
 				xhttp.open("DELETE", this.APIAddress + resource, true);
 				xhttp.send();
-				return await new Promise(function(resolve, reject) {
-					xhttp.onreadystatechange = function() {
+				return await new Promise(function (resolve, reject) {
+					xhttp.onreadystatechange = function () {
 						resolve(xhttp.status);
 					};
 				});
+			} else {
+				const fetch = require("node-fetch");
 
+				let response = await fetch(this.APIAddress + resource, {
+					headers: {
+						Authorization: this.APIKey
+					},
+					method: "DELETE"
+				});
+				return await new Promise(function (resolve, reject) {
+					resolve(response.status);
+				});
 			}
-
 		} catch (error) {
 			throw error;
 		}
@@ -114,27 +147,40 @@
 			if (typeof fetch === "function") {
 				let response = await fetch(this.APIAddress + resource, {
 					headers: {
-						"Authorization": this.APIKey,
+						Authorization: this.APIKey,
 						"Content-Type": "application/json"
 					},
 					method: "PUT",
 					body: JSON.stringify(data)
 				});
-	
+
 				return await response.json();
-			} else {
+			} else if (typeof XMLHttpRequest === "function") {
 				let xhttp = new XMLHttpRequest();
 				xhttp.open("PUT", this.APIAddress + resource, true);
 				xhttp.setRequestHeader("Content-Type", "application/json");
 				xhttp.setRequestHeader("Authorization", this.APIKey);
 				xhttp.send(JSON.stringify(data));
-				return await new Promise(function(resolve, reject) {
-					xhttp.onreadystatechange = function() {
+				return await new Promise(function (resolve, reject) {
+					xhttp.onreadystatechange = function () {
 						if (this.readyState == 4 && this.status == 200) {
 							resolve(JSON.parse(xhttp.responseText));
 						}
 					};
 				});
+			} else {
+				const fetch = require("node-fetch");
+
+				let response = await fetch(this.APIAddress + resource, {
+					headers: {
+						Authorization: this.APIKey,
+						"Content-Type": "application/json"
+					},
+					method: "PUT",
+					body: JSON.stringify(data)
+				});
+
+				return await response.json();
 			}
 		} catch (error) {
 			throw error;
@@ -147,5 +193,5 @@
 		post,
 		del,
 		put
-	}
-}));
+	};
+});
